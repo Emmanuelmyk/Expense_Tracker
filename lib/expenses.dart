7c517bb1c485464c707cebf50ca4f9618144dfe4
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-04-04 15:21:10 +0100

    added listView widget for long list

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +11,20 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      date: DateTime.now(),
+      amount: 20.22,
+      category: Category.work,
+    ),
+    Expense(
+      title: 'movies',
+      date: DateTime.now(),
+      amount: 34.22,
+      category: Category.leisure,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
