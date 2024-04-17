4d16e406ab347639071988ffce50625bf84330d7
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-04-17 17:55:46 +0100

    update

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,7 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -46,21 +46,31 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('modal buttom sheets'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        titleTextStyle: const TextStyle(
+            color: Colors.white, fontSize: 25, fontWeight: FontWeight.w700),
         backgroundColor: const Color.fromARGB(255, 207, 51, 51),
         actions: [
           IconButton(
-              onPressed: () {},
+              onPressed: _openAddExpenseOverlay,
               icon: const Icon(color: Colors.white, Icons.add)),
         ],
       ),
       body: Center(
         child: Column(
           children: [
-            const Text('charts'),
+            const Text('The Charts'),
             Expanded(
               child: ExpensesList(expenses: _registeredExpenses),
             )
