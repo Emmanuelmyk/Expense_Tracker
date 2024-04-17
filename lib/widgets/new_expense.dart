a35dc42b7c232200329b3efeac80c328af7dbf5f
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-04-17 21:09:58 +0100

    update

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -9,18 +9,52 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 20,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            maxLength: 20,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: const Text('Save Expense'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Delete'),
+              ),
+            ],
+          )
         ],
       ),
     );
