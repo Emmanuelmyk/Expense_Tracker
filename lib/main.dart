c161c0439640c39bb53af4d6673f2f7ec3db3994
Emmanuel Mbah <emmanuelmbah91@gmail.com>
2024-09-17 20:09:01 +0100

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: const Color.fromARGB(255, 96, 59, 101),
+  seedColor: const Color.fromARGB(255, 0, 255, 51),
 );
 
 void main() {
@@ -10,9 +10,25 @@ void main() {
     MaterialApp(
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
-        appBarTheme: AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer),
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.normal,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 20,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
--- lib/widgets/extenses_list/expense_item.dart
+++ lib/widgets/extenses_list/expense_item.dart
@@ -11,8 +11,8 @@ class ExpenseItem extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 60,
+          horizontal: 30,
+          vertical: 20,
         ),
         child: Column(
           children: [
